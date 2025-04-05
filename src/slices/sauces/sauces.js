a968830f30fd31b8dd71d8bf73cbca923dfb343c
NedPov <nedusenkop@gmail.com>
2025-04-05 16:23:55 +0600

    saucesSlice

---
+++ src/slices/sauces/sauces.js
@@ -0,0 +1,52 @@
+
+import axios from "axios";
+
+const SERVER_URL = 'http://localhost:9876/sauces';
+
+// Настройка axios по дефолту
+const api = axios.create({
+    baseURL: SERVER_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    //работа с куки(headers), отправляем токен
+    withCredentials: true
+});
+
+
+// Запрос: Получение всех соусов
+export const fetchSauces = async () => {
+    const response = await api.get('/'); 
+    return response.data;
+};
+
+// Запрос: Добавить соус
+export const addSauces = async ({title, description, price }) => {
+    const response = await api.post('/', {title, description, price});
+    console.log(response.data);
+    return response.data;
+};
+
+// Запрос: Изменение соуса
+export const editSauces = async (title, description, price, saucesId) => {
+    const response = await api.put(`/${saucesId}/edit`, { title, description, price });
+    return response.data;
+};
+
+// Запрос: Удаление соуса
+export const deleteSauces = async (id) => {
+    const response = await api.delete(`/${id}`);
+    return response.data.id;
+};
+
+
+
+// interceptors - фишка axios. Перехватывает запросы исходящие и добавляет в них то, что пропишем
+api.interceptors.request.use(async (config) => {
+    // Автоматом отправляем наш токен со всеми запросами
+    let token = localStorage.getItem('accessToken');
+    // Настраиваем что в заголовке Authorization будет наш токен доступа
+    config.headers.Authorization = `Bearer ${token}`;
+    
+    return config;
+});
--- src/slices/sauces/saucesSlice.js
+++ src/slices/sauces/saucesSlice.js
@@ -1,15 +1,38 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
-// ПОЛУЧЕНИЕ СОУСОВ
-export const loadSauces = createAsyncThunk('sauces/loadSauces', async () =>{
-    return [{title: "sauces1", description: "qwerty", id: 1, price: '1850'}, {title: "sauces2", description: "asdfgh", id: 2, price: '1300'}, {title: "sauces3", description: "zxcvbn", id: 3, price: '1150'},];
+import { fetchSauces, addSauces, editSauces, deleteSauces } from "./sauces";
+
+
+// FETCH ЗАПРОСЫ
+// =========================================
+// Получение всех соусов
+export const loadSauces = createAsyncThunk('sauces/loadSauces', async () => {
+    return await fetchSauces();
 });
 
+// Добавление соуса
+export const addFetchSauces = createAsyncThunk('sauces/addFetchSauces', async ({ title, description, price }) => {
+    return await addSauces({ title, description, price });
+});
+
+// отправка на сервер измененного соуса
+export const editFetchSauces = createAsyncThunk('sauces/editFetchSauces', async (title, description, price, saucesId) => {
+    return await editSauces({ title, description, price, saucesId });
+});
+
+// Удаление соуса
+export const deleteFetchSauces = createAsyncThunk('sauces/deleteFetchSauces', async (id) => {
+    return await deleteSauces(id);
+});
+// =========================================
 
 
+// Начальное состояние
 const initialState = {
     sauces: [],
+    error: '',
+    message: '',
 };
 
 
@@ -19,8 +42,33 @@ const saucesSlice = createSlice({
     reducers: {},
     extraReducers: (builder) => {
         builder
+            // Получение всех соусов
             .addCase(loadSauces.fulfilled, (state, action) => {
                 state.sauces = action.payload;
+            })
+            .addCase(loadSauces.rejected, (state, action) => {
+                state.error = action.payload;
+            })
+            // Добавление соуса
+            .addCase(addFetchSauces.fulfilled, (state, action) => {
+                state.sauces.push(action.payload);
+            })
+            .addCase(addFetchSauces.rejected, (state, action) => {
+                state.error = action.payload;
+            })
+            // Изменение соуса
+            .addCase(editFetchSauces.fulfilled, (state, action) => {
+                // state.sauces.push(action.payload);
+            })
+            .addCase(editFetchSauces.rejected, (state, action) => {
+                state.error = action.payload;
+            })
+            // Удаление соуса
+            .addCase(deleteFetchSauces.fulfilled, (state, action) => {
+                state.sauces = state.sauces.filter(sauce => sauce.id !== action.payload);
+            })
+            .addCase(deleteFetchSauces.rejected, (state, action) => {
+                state.error = action.payload;
             })
     }
 });
